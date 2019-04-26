38d727fd305cbf768b702990a2b2e487b644ca84
Mick Evans <jaspercayne@gmail.com>
2019-04-26 14:07:06 -0400

    feat: add recipecard component
    Recipes can be displayed on one of these cards, with a demo recipe displayed on the home page

--- src/app/pages/recipecard/recipecard.component.ts
+++ src/app/pages/recipecard/recipecard.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipecard.component.scss']
 })
 export class RecipecardComponent implements OnInit {
+  title: string;
+  author: string;
+  category: string;
+  ingredients: string[];
+  directions: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.title = 'Peanut Butter and Jelly Sandwich';
+    this.author = 'Admin';
+    this.category = 'Lunch';
+    this.ingredients = ['bread', 'peanut butter', 'jelly'];
+    this.directions = ['spread peanut butter on one slice', 'spread jelly on the other', 'stick them together'];
   }
 
 }
