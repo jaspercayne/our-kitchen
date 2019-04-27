3cc1be7a58a643f0d4ae8d02e2a8534a90f22dd5
Mick Evans <jaspercayne@gmail.com>
2019-04-26 21:24:00 -0400

    adjust recipe card for new model

--- src/app/pages/recipecard/recipecard.component.ts
+++ src/app/pages/recipecard/recipecard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from 'src/app/shared/recipe.model';
 
 @Component({
   selector: 'app-recipecard',
@@ -6,20 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipecard.component.scss']
 })
 export class RecipecardComponent implements OnInit {
-  title: string;
-  author: string;
-  category: string;
-  ingredients: string[];
-  directions: string[];
+  @Input() recipe: Recipe;
 
   constructor() { }
 
   ngOnInit() {
-    this.title = 'Peanut Butter and Jelly Sandwich';
-    this.author = 'Admin';
-    this.category = 'Lunch';
-    this.ingredients = ['bread', 'peanut butter', 'jelly'];
-    this.directions = ['spread peanut butter on one slice', 'spread jelly on the other', 'stick them together'];
+    // this.recipe.title = 'Peanut Butter and Jelly Sandwich';
+    // this.recipe.author = 'Admin';
+    // this.recipe.category = 'Lunch';
+    // this.recipe.ingredients = ['bread', 'peanut butter', 'jelly'];
+    // this.recipe.directions = ['spread peanut butter on one slice', 'spread jelly on the other', 'stick them together'];
   }
 
 }
