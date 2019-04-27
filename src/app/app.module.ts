bde564b4632275ca40b92047ae98b8f7196c8ca2
Mick Evans <jaspercayne@gmail.com>
2019-04-26 21:24:30 -0400

    feat: add reactive forms and upload component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,19 +9,24 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { HomeComponent } from './pages/home/home.component';
 import { RecipecardComponent } from './pages/recipecard/recipecard.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { UploadrecipeComponent } from './pages/uploadrecipe/uploadrecipe.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RecipecardComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    UploadrecipeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
