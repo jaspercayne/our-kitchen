7b82eb98162d7add6728b01d587abcf579c8ab55
Mick Evans <jaspercayne@gmail.com>
2019-04-28 09:18:50 -0400

    chore: add firebase config to repo
    This is a safe action. The database rules are secured and travis needs this

---
+++ src/firebase.ts
@@ -0,0 +1,8 @@
+export const Firebase = {
+    apiKey: 'AIzaSyBjNFptn96yT8KGD8olmBic30PmT3cU-4M',
+    authDomain: 'playground-7387a.firebaseapp.com',
+    databaseURL: 'https://playground-7387a.firebaseio.com',
+    projectId: 'playground-7387a',
+    storageBucket: 'playground-7387a.appspot.com',
+    messagingSenderId: '633650962496'
+}
