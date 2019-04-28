efe59fcf4f748c73860762ba0a1b3d51a7115130
Mick Evans <jaspercayne@gmail.com>
2019-04-28 18:50:28 -0400

    chore: set new project config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,8 @@
 export const Firebase = {
-    apiKey: 'AIzaSyBjNFptn96yT8KGD8olmBic30PmT3cU-4M',
-    authDomain: 'playground-7387a.firebaseapp.com',
-    databaseURL: 'https://playground-7387a.firebaseio.com',
-    projectId: 'playground-7387a',
-    storageBucket: 'playground-7387a.appspot.com',
-    messagingSenderId: '633650962496'
-}
+    apiKey: 'AIzaSyBMPBLPhXcnVdGrIpP29-gcigcmCFggy0A',
+    authDomain: 'our-kitchen-f4ecd.firebaseapp.com',
+    databaseURL: 'https://our-kitchen-f4ecd.firebaseio.com',
+    projectId: 'our-kitchen-f4ecd',
+    storageBucket: 'our-kitchen-f4ecd.appspot.com',
+    messagingSenderId: '944203585319'
+};
