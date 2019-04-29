32bf0c388a11962cb42af46f3e3575fda6cb7332
Mick Evans <jaspercayne@gmail.com>
2019-04-29 12:58:40 -0400

    feat: add autocomplete module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,7 +11,8 @@ import {
   MatGridListModule,
   MatStepperModule,
   MatFormFieldModule,
-  MatInputModule
+  MatInputModule,
+  MatAutocompleteModule
 } from '@angular/material';
 
 @NgModule({
@@ -26,7 +27,8 @@ import {
     MatGridListModule,
     MatStepperModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatAutocompleteModule
   ],
   exports: [
     MatButtonModule,
@@ -39,7 +41,8 @@ import {
     MatGridListModule,
     MatStepperModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatAutocompleteModule
   ]
 })
 export class MaterialModule { }
