3c5e13ec801a92d2988eb1cfa403cdb96de72a89
Mick Evans <jaspercayne@gmail.com>
2019-04-30 14:42:08 -0400

    feat: recipes are now loaded by the route id

--- src/app/pages/recipe-detail/recipe-detail.component.ts
+++ src/app/pages/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/shared/recipe.model';
+import { RecipesService } from 'src/app/shared/recipes.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent implements OnInit {
+  recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipesService,private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.recipe = this.recipeService.getRecipe(id);
+    console.log(id);
   }
 
 }
