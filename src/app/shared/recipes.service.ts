6c38ff3c146bb405e7b18c8bc30d3eca1df91e03
Mick Evans <jaspercayne@gmail.com>
2019-05-10 12:26:38 -0400

    feat: use ratings document/code cleanup

--- src/app/shared/recipes.service.ts
+++ src/app/shared/recipes.service.ts
@@ -20,6 +20,7 @@ export class RecipesService implements OnInit {
 
   recipeDoc: AngularFirestoreDocument<Recipe>;
   recipe: Observable<Recipe>;
+  rdoc;
 
   val: number[] = [];
 
@@ -35,9 +36,6 @@ export class RecipesService implements OnInit {
       });
   }
 
-  /**
-   * createRecipe
-   */
   public createRecipe(workingRecipe: Recipe) {
     this.afs.collection('recipes').add({
       title: workingRecipe.title,
@@ -57,9 +55,9 @@ export class RecipesService implements OnInit {
       author: this.auth.afAuth.auth.currentUser.displayName,
       category: workingRecipe.category,
       ingredients: workingRecipe.ingredients,
-      directions: workingRecipe.directions
+      directions: workingRecipe.directions,
+      rating: -1
     });
-    // TODO add ratings doc here
     this.afs.collection('ratings/').doc(newid).set({
       one: 0,
       two: 0,
@@ -69,78 +67,68 @@ export class RecipesService implements OnInit {
     });
   }
 
-  /**
-   * getRecipe
-   */
   public getRecipe(recipeId) {
     return this.afs.doc('/recipes/' + recipeId).valueChanges();
   }
 
   public getRecipeRating(recipeId): number[] {
-    this.afs.doc('/ratings/' + recipeId)
-      .valueChanges()
-      .subscribe(res => {
-        this.val[0] = res['one'.toString()];
-        this.val[1] = res['two'.toString()];
-        this.val[2] = res['three'.toString()];
-        this.val[3] = res['four'.toString()];
-        this.val[4] = res['five'.toString()];
-      });
-    return this.val;
+    if (recipeId !== null) {
+      this.afs.doc('/ratings/' + recipeId)
+        .valueChanges()
+        .subscribe(res => {
+          this.val[0] = res['one'.toString()];
+          this.val[1] = res['two'.toString()];
+          this.val[2] = res['three'.toString()];
+          this.val[3] = res['four'.toString()];
+          this.val[4] = res['five'.toString()];
+        });
+      return this.val;
+    }
   }
 
   public updateRecipeRating(recipe, newRating) {
     let myRatings: number[] = [0, 0, 0, 0, 0];
     myRatings = this.getRecipeRating(recipe.recipeid);
-    switch (newRating) {
-      case 1:
-        const one = myRatings[0] + 1;
-        this.afs.doc('/ratings/' + recipe.recipeid).update({ one });
-        break;
-      case 2:
-        const two = myRatings[1] + 1;
-        this.afs.doc('/ratings/' + recipe.recipeid).update({ two });
-        break;
-      case 3:
-        const three = myRatings[2] + 1;
-        this.afs.doc('/ratings/' + recipe.recipeid).update({ three });
-        break;
-      case 4:
-        const four = myRatings[3] + 1;
-        this.afs.doc('/ratings/' + recipe.recipeid).update({ four });
-        break;
-      case 5:
-        const five = myRatings[4] + 1;
-        this.afs.doc('/ratings/' + recipe.recipeid).update({ five });
-        break;
-      default:
-        break;
+    if (myRatings !== null) {
+      switch (newRating) {
+        case 1:
+          const one = myRatings[0] + 1;
+          this.afs.doc('/ratings/' + recipe.recipeid).update({ one });
+          break;
+        case 2:
+          const two = myRatings[1] + 1;
+          this.afs.doc('/ratings/' + recipe.recipeid).update({ two });
+          break;
+        case 3:
+          const three = myRatings[2] + 1;
+          this.afs.doc('/ratings/' + recipe.recipeid).update({ three });
+          break;
+        case 4:
+          const four = myRatings[3] + 1;
+          this.afs.doc('/ratings/' + recipe.recipeid).update({ four });
+          break;
+        case 5:
+          const five = myRatings[4] + 1;
+          this.afs.doc('/ratings/' + recipe.recipeid).update({ five });
+          break;
+        default:
+          break;
+      }
     }
-
   }
 
-  /**
-   * getRecipes
-   */
   public getRecipes() {
     return this.afs.collection('/recipes').valueChanges();
   }
 
-  /**
-   * updateRecipe
-   */
-  public updateRecipe(recipe) {
-    console.log(recipe + ' updated');
+  public updateRecipe(recipe: Recipe) {
     this.recipeCollection.doc(recipe.recipeid).set({ recipe }).then(() => {
-      console.log('Document successfully updated!');
+      console.log(recipe.title + ' successfully updated!');
     }).catch((error) => {
       console.error('Error updating document: ', error);
     });
   }
 
-  /**
-   * deleteRecipe
-   */
   public deleteRecipe(recipeId: string) {
     this.recipeCollection.doc(recipeId).delete().then(() => {
       console.log('Document successfully deleted!');
